ff0f019a237c8b342081f9d443b3110a211b6e29
MarianaHerrr <127801626+MarianaHerrr@users.noreply.github.com>
2023-04-11 22:54:28 -0600

    Delete tncar directory

--- tncar/src/main/java/org/tncar/tncar/repository/CategoriasRepository.java
+++
@@ -1,8 +0,0 @@
-package org.tncar.tncar.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.tncar.tncar.entity.Categoria;
-
-public interface CategoriasRepository extends JpaRepository<Categoria, Integer> {
-
-}
